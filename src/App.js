ae62c61cfb83aacfc18cd66b59f1b6d20a309fa3
Guilherme J. Floyd <gjpnkflyd@gmail.com>
2020-08-21 16:18:30 -0300

    DONE

--- src/App.js
+++ src/App.js
@@ -1,30 +1,74 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { FaGithub } from 'react-icons/fa'
+import api from 'services/api';
 
-import "./styles.css";
+import './styles.css';
+
+import Header from 'components/Header';
 
 function App() {
+  const [ repositories, setRepositories ] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    })
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: `Novo repositório no Github`,
+      url: 'https://github.com/guigawhata',
+      techs: ['Laravel', 'Node.js', 'React.js', 'React Native']
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const deleteRepository = await api.delete(`repositories/${id}`);
+
+    if (deleteRepository) {
+      const index = repositories.findIndex(repository => repository.id === id);
+
+      repositories.splice(index, 1);
+
+      setRepositories([...repositories]);
+    }
   }
 
   return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+    <>
+    <Header />
+    <div className="repositories-cont">
+      <div className="list-cont">
+        <ul data-testid="repository-list">
+          {repositories.map(repository => 
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
+          <li key={repository.id}>
+            <div className="info">
+              <h3>{repository.title}</h3>
 
-      <button onClick={handleAddRepository}>Adicionar</button>
+              <a href={repository.url} target="_blank" rel="noopener noreferrer">
+                <FaGithub size={40}/>
+              </a>
+            </div>
+
+            <div className="remove-cont">
+              <button className="button-remove" onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+            </div>            
+          </li>
+                    
+          )}          
+        </ul>               
+      </div>
+      <button className="button-add" onClick={handleAddRepository}>Adicionar</button>
     </div>
+    </>
   );
 }
 
---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.css';
+
+function Header() {
+  return (
+    <div className="header-cont">
+      <img className="devfloyd-logo" src="https://storage.googleapis.com/devfloyd/logo.devfloyd.light.png" alt="dev.floyd"></img>    
+    </div>
+  );
+}
+
+export default Header;
